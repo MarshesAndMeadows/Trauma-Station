madeline-celeste/Maidstation

change antag pity system (#5068)
rah

Co-authored-by: Rouden <149893554+Roudenn@users.noreply.github.com>

Content.Server/Antag/AntagSelectionSystem.cs
@@ -355,7 +355,11 @@ public sealed partial class AntagSelectionSystem : GameRuleSystem<AntagSelection
 
         // weight by playtime since last rolled
         foreach (var se in pool)
-            weights[se] = (float)(_playTime.GetOverallPlaytime(se) - _lastRolled.GetLastRolled(se.UserId)).TotalSeconds;
+        {
+            var lastRoll = (float)(_playTime.GetOverallPlaytime(se) - _lastRolled.GetLastRolled(se.UserId)).TotalSeconds;
+            //weight clamped between 5 hours and 20 hours
+            weights[se] = float.Clamp(lastRoll, 18000.0f, 72000.0f);
+        }
 
         return weights;
     }

MarshesAndMeadows/Trauma-Station

Content.Server/Antag/AntagSelectionSystem.cs
@@ -275,7 +275,11 @@ public sealed partial class AntagSelectionSystem : GameRuleSystem<AntagSelection
 
         // weight by playtime since last rolled
         foreach (var se in pool)
-            weights[se] = (float)(_playTimeMan.GetOverallPlaytime(se) - _lastRolled.GetLastRolled(se.UserId)).TotalSeconds;
+        {
+            var lastRoll = (float)(_playTimeMan.GetOverallPlaytime(se) - _lastRolled.GetLastRolled(se.UserId)).TotalSeconds;
+            //weight clamped between 5 hours and 20 hours
+            weights[se] = float.Clamp(lastRoll, 18000.0f, 72000.0f);
+        }
 
         return weights;
     }